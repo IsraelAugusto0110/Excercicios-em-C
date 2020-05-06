f0f3117e1639a71c4a6d060d64a85a52d8dcfb99
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-05-05 22:08:55 -0300

    up

--- ListaVetores/ExVetores08.c
+++ ListaVetores/ExVetores08.c
@@ -6,20 +6,24 @@ void main() {
     estão com a nota acima de 5.0. Obs.: Se nenhum aluno tirou nota acima de 5.0,
     imprimir mensagem: Não há nenhum aluno com nota acima de 5.**/
 
-    int n;
+    int n, i, j;
 
     scanf("%d", &n);
 
-    int notas[n][n],x, i, j, media, soma = 0;
+    float notas[n][n],x, media, soma = 0;
 
     for(i = 0; i < n; i++){
         for(j = 0; j < n; j++){
-            printf("aluno [%d] nota[%d]: ", i, j);
-            scanf("%d", &x);
-            nota[i][j] = x;
+            printf("aluno [%d] nota [%d]: ", i, j);
+            scanf("%f", &x);
+            notas[i][j] = x;
             /**neste caso, cada linha[i] representa um aluno
             e as colunas[j] as notas**/
             soma = soma + x;
         }
     }
+
+    media = soma / (n*n);
+
+    printf("a media das notas e %.1f", media);
 }
