cec560149db9eb75c90268b10847773d18f66139
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-05-06 09:38:43 -0300

    novo

---
+++ ListaVetores/ExVetores10.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+void main() {
+    /**10. Deseja-se publicar o número de acertos de cada aluno em uma prova em forma de
+    testes. A prova consta de 30 questões, cada uma com cinco alternativas
+    identificadas por A, B, C, D e E. Para isso são dados:
+    a. o cartão gabarito;
+    b. o número de alunos da turma;
+    c. o cartão de respostas para cada aluno, contendo o seu número e suas
+    respostas.**/
+
+    for(i = 1; i < n; i++){
+    }
+}
