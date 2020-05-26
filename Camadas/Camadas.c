f0a6f0a40aabd9eeef3f734d4835e826f087a34f
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-05-26 16:29:28 -0300

    correção

--- Camadas/Camadas.c
+++ Camadas/Camadas.c
@@ -1,33 +1,26 @@
 #include<stdio.h>
-
 void main(){
-    int n;
+    int n, i, j;
     printf("Quantas camadas? ");
     scanf("%d", &n);
 
-    int layers = (2*n) + 1; // notei que a quantidade de camadas é 2 * N + 1;
-                            //por exemplo n = 3 temos 7 linhas e 7 colunas
-
-    char vetor[layers][layers];
-    int i, j;
     char l = 'A';
+    char vetor[n][n];
 
-    /*por enquanto este loop serve para preencher apenas com 'A'
-      uma matriz com 2*n + 1 colunas e 2*n + linhas*/
-    for(i = 0; i < layers; i++){
-        for(j = 0; j < layers; j++){
-            if(i == n && j == n){
-                vetor[i][j] = '*';
-                /*apos n linhas e n colunas desenha o '*'*/
-            }else{
-                vetor[i][j] = l;
-            }
-            printf("%c ", vetor[i][j]);
-            if(i == j && i > 0){
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            vetor[i][j] = l;
+            if(i == 1 && j == 0){
                 l++;
-            }else{
-                l = 'A';
+            }else if(i == n || j == n){
+                l--;
             }
+        }
+    }
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            printf("%c ", vetor[i][j]);
         }
         printf("\n");
     }
