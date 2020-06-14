a171c7bff11810bcf68b4bb232c9fb6ed1417c87
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-06-14 14:22:24 -0300

    ok

--- Recorrencia/reco.c
+++ Recorrencia/reco.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<math.h>
+#include <math.h>
 
 void main(){
     float pi = 3.1415, R, soma = 0;
@@ -9,16 +9,16 @@ void main(){
     printf("X: ");
     scanf("%d", &x);
 
-    R = (x * pi)/180;
+    R = ((x * pi)/180);
 
-    R = (pow(R,2));
+    R *= R;
 
     do{
         soma += R/b;
-        R *= (-pow(R, 2));
-        b = ((2 * i) + 2) * ((2*i) + 1) * b;
+        R *= -(pow(R, 2)* i);
+        b *= ((2 * i) + 2) * ((2*i) + 1);
         i++;
     }while(i<=5);
 
-    printf("Taylor(%d) = %.3f\n", x, soma);
+    printf("Soma = %.3f\n", soma);
 }
