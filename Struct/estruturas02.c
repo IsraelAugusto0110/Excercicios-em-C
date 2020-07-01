8241878009563c046a54b9db3bf2575e49ab471c
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2020-07-01 20:02:23 -0300

    novo

--- Struct/estruturas02.c
+++ Struct/estruturas02.c
@@ -26,17 +26,21 @@ dma fim_evento(dma datainicio, int duracao){
     dma datafim;
     int anos, meses, dias;
 
-    anos = duracao/365;
-    meses = (duracao%365)/30;
-    dias = (duracao%365)%30;
+    anos = duracao/360;//se todos os 12 meses tivessem exatamente 30 dias
+    meses = (duracao%360)/30;
+    dias = (duracao%360)%30;
 
     if(datainicio.dia + dias > 30){
         meses += 1;
         dias = (datainicio.dia + dias) - 30;
+    }else{
+        dias = (datainicio.dia + dias);
     }
     if(datainicio.mes + meses > 12){
         anos = anos + 1;
         meses = (datainicio.mes + meses) - 12;
+    }else{
+        meses = (datainicio.mes + meses);
     }
 
     anos = datainicio.ano + anos;
